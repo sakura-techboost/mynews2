a58e170523fa2cf078600cdef05ce857e84168ad
sakura <cherry.m-love4@ezweb.ne.jp>
2020-04-24 13:35:20 +0000

    retake

--- app/Http/Controllers/Admin/profileController.php
+++ app/Http/Controllers/Admin/profileController.php
@@ -8,4 +8,20 @@ use App\Http\Controllers\Controller;
 class profileController extends Controller
 {
     //
+    public function add()
+    {
+        return view('admin.profile.create');
+    }
+    public function create()
+    {
+        return redirect('admin/profile/create');
+    }
+    public function edit()
+    {
+        return view('admin.profile.edit');
+    }
+    public function update()
+    {
+        return redirect('admin/profile/edit');
+    }
 }
