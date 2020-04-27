2c1889724317ea860d86af2f84b30dbadf8f0b8c
sakura <cherry.m-love4@ezweb.ne.jp>
2020-04-27 07:42:19 +0000

    phplaravel11Task

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,24 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X=UA=Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        
-        <title>MyNews</title>
-    </head>
-    <body>
-        <h1>プロフィール作成画面</h1>
-    </body>
-</html>
+{{--4. 【応用】 プロフィール作成画面用に、--}}
+{{--resources/views/admin/profile/create.blade.php ファイルを作成し、--}}
+{{--3. で作成した profile.blade.phpファイルを読み込み、また --}}
+{{--プロフィールのページであることがわかるように titleとcontentを編集しましょう。--}}
+{{--（ヒント: resources/views/admin/news/create.blade.php を参考にします。）--}}
+
+
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に埋め込む --}}
+@section('title', 'プロフィールの新規作成')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>Myプロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
