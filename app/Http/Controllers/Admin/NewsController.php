cc20c56c441937f2b6c2cc6b2d7751a3901eeb4d
sakura <cherry.m-love4@ezweb.ne.jp>
2020-05-02 08:53:18 +0000

    PHPLaravel14Task

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\News;
+
 
 class NewsController extends Controller
 {
@@ -14,6 +16,25 @@ class NewsController extends Controller
     }
     public function create(Request $request)
     {
+        // Varidationを行う
+        $this->validate($request,News::$rules);
+        $news=new News;
+        $form=$request->all();
+        // フォームから画像が送信されてきたら保存して、$news->image_pathに画像のパスを保存する
+        if(isset($form['image'])){
+            $path=$request->file('image')->store('public/image');
+            $news->image_path=basename($path);
+        }else{
+            $news->image_path=null;
+        }
+        // フォームから送信されてきた_tokenを削除する
+        unset($form['_token']);
+        // フォームから送信されてきたimageを削除する
+        unset($form['image']);
+        
+        //データベースに保存する
+        $news->fill($form);
+        $news->save();
         // ニュース新規投稿画面にリダイレクトする
         return redirect('admin/news/create');
     }
