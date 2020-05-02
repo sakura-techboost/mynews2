a11a021fcf590c7e4dc005f1563a86d12d311bab
sakura <cherry.m-love4@ezweb.ne.jp>
2020-05-02 06:47:12 +0000

    PHPLaravel13Task

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -12,4 +12,9 @@ class NewsController extends Controller
     {
         return view('admin.news.create');
     }
+    public function create(Request $request)
+    {
+        // ニュース新規投稿画面にリダイレクトする
+        return redirect('admin/news/create');
+    }
 }
